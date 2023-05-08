7737ceb729daca6a79b218c718df5c59e042fb64
ASUS <ghassenhlaoui@gmail.com>
2023-05-08 11:00:03 +0200

    Add project files.

---
+++ MyBookStore/Repositories/Implementation/AuthorService.cs
@@ -0,0 +1,68 @@
+﻿using MyBookStore.Models.Domain;
+using MyBookStore.Repositories.Abstract;
+
+namespace MyBookStore.Repositories.Implementation
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly DatabaseContext context;
+        public AuthorService(DatabaseContext context)
+        {
+            this.context = context;
+        }
+        public bool Add(Author model)
+        {
+            try
+            {
+                context.Author.Add(model);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var data = this.FindById(id);
+                if (data == null)
+                    return false;
+                context.Author.Remove(data);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public Author FindById(int id)
+        {
+            return context.Author.Find(id);
+        }
+
+        public IEnumerable<Author> GetAll()
+        {
+            return context.Author.ToList();
+        }
+
+        public bool Update(Author model)
+        {
+            try
+            {
+                context.Author.Update(model);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
